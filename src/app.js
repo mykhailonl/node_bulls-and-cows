a8c305190570ebe1cf74bee926dd15ea3614592c
Mykhailo Lapchynskyi <mykhailolapchynskyi@Dikaas-MacBook-Pro.local>
2024-10-21 14:15:32 +0200

    add task solution

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// Write your code here
+import readline from 'readline';
+import { generateRandomNumber } from './modules/generateRandomNumber';
+import { checkIsValidUserInput } from './modules/checkIsValidUserInput';
+import { getBullsAndCows } from './modules/getBullsAndCows';
+
+const terminal = readline.createInterface(process.stdin, process.stdout);
+
+const numberToGuess = generateRandomNumber();
+
+terminal.question('Guess the number', (userNumber) => {
+  if (!checkIsValidUserInput(userNumber)) {
+    return;
+  }
+
+  if (userNumber === numberToGuess) {
+    terminal.close();
+  } else {
+    getBullsAndCows(userNumber, numberToGuess);
+  }
+});
